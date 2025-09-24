1193a9f2490b19ecbd05e0234388ac52aec6d2ba
seharijaz15 <seharijaz15@gmail.com>
2025-09-24 10:36:01 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+class RainSimulation
+{
+    static void Main()
+    {
+        string[] frames = new string[]
+        {
+@"
+     . . . . . . . . . . . . . . . . .
+     , , , , , , , , , , , , , , , , ,
+     , , , , , , , , , , , , , , , , ,
+           ~ ~ ~  A S H F A  ~ ~ ~
+     ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' '
+     . . . . . . . . . . . . . . . . .",
+@"
+     , , , , , , , , , , , , , , , , ,
+     . . . . . . . . . . . . . . . . .
+           ~ ~ ~  A S H F A  ~ ~ ~
+     , , , , , , , , , , , , , , , , ,
+     ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' '
+     . . . . . . . . . . . . . . . . .",
+@"
+     ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' '
+     . . . . . . . . . . . . . . . . .
+           ~ ~ ~  A S H F A  ~ ~ ~
+     , , , , , , , , , , , , , , , , ,
+     . . . . . . . . . . . . . . . . .
+     ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' '",
+@"
+     . . . . . . . . . . . . . . . . .
+     ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' '
+           ~ ~ ~  A S H F A  ~ ~ ~
+     . . . . . . . . . . . . . . . . .
+     , , , , , , , , , , , , , , , , ,
+     ' ' ' ' ' ' ' ' ' ' ' ' ' ' ' '"
+        };
+
+        Console.CursorVisible = false;
+
+        for (int i = 0; i < 150; i++)
+        {
+            foreach (string frame in frames)
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.Write(frame);
+                Thread.Sleep(120); // Slower for elegance
+            }
+        }
+
+        Console.CursorVisible = true;
+    }
+}
